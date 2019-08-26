dd0006e14249749faf366dcca9cc6190680a0d93
atn19808 <atn1908@outlook.com>
2019-08-26 12:55:43 -0500

    add header section and collection preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopePage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopePage} />
+      </Switch>
     </div>
   );
 }
